e71b26799f6c541f0f1a9ae487bf813ba9d85909
Marcello Bernardes <mnunesbernardes@gmail.com>
2023-11-07 17:30:20 -0500

    exercise1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -14,16 +14,8 @@ const Part = ({name, exercises}) => {
     </div>
   );
 }
-const Content =(props) => {
-  return(
-    <div>
-      <Part name={props.part1name} exercises={props.part1exercises}/>
-      <Part name={props.part2name} exercises={props.part2exercises}/>
-      <Part name={props.part3name} exercises={props.part3exercises}/>
-    </div>
-  );
-}
-/* const Content =({parts}) => {
+
+const Content =({parts}) => {
   return(
     <div>
       <Part name={parts[0].name} exercises={parts[0].exercises}/>
@@ -31,7 +23,7 @@ const Content =(props) => {
       <Part name={parts[2].name} exercises={parts[2].exercises}/>
     </div>
   );
-} */
+}
 
 const All = ({exercises}) =>{
   return(
@@ -45,25 +37,27 @@ const All = ({exercises}) =>{
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
-  const totalExercises = part1.exercises + part2.exercises + part3.exercises;
+  const totalExercises = parts[0].exercises + parts[1].exercises + parts[2].exercises;
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1name={part1.name} part1exercises={part1.exercises} part2name={part2.name} part2exercises={part2.exercises} part3name={part3.name} part3exercises={part3.exercises}/>
+      <Content parts={parts}/>
       <All exercises={totalExercises} />
     </div>
   )
