a7a1825b7e7fbc138a28ab006e8e12ead696c158
Marcello Bernardes <mnunesbernardes@gmail.com>
2023-11-08 16:03:12 -0500

    exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -32,33 +32,35 @@ const All = ({parts}) =>{
       <p>Number of exercises: {totalExercises}</p>
     </div>
   );
-} 
+}
 
 
 const App = () => {
 
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
-      <All parts={parts} />
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <All parts={course.parts} />
     </div>
   )
 }
