030d17d82d1c7cb9706998243c6bf4b3aaa4b1ed
Marcello Bernardes <mnunesbernardes@gmail.com>
2023-12-07 18:25:15 -0500

    Exercise 2.1

---
+++ part2/courseinfo/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Course = ({course})=> {
+
+    const Header = ({ course }) => <h1>{course.name}</h1>
+    
+    const Part = ({ part }) => 
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    
+    const Content = ({ parts }) => 
+      <>
+        <Part
+          part={parts[0]} 
+        />
+        <Part
+          part={parts[1]} 
+        />
+        <Part
+          part={parts[2]} 
+        />      
+      </>
+
+return (
+    <div>
+        <Header course={course}/>
+        <Content parts={course.parts}/>
+    </div>
+)
+}
+
+export default Course
