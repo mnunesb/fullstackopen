18f34c33dcf4665afa128b8f4b5f3f951cfb7bc5
Marcello Bernardes <mnunesbernardes@gmail.com>
2023-12-27 18:03:15 -0500

    Exercise 2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,22 +2,29 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
+    {name: 'Arto Hellas', number: 2498771125}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) =>{
     event.preventDefault();
     if(find(newName) === true || (newName) === ''){
       alert("name is empty or is already included")
       setNewName('')
-      return(0);
-    } else{
+      return(0);}
+        if(find(newNumber) === true || (newNumber) === ''){
+          alert("Number is empty or is already included")
+          setNewNumber('')
+          return(0);
+      } else{
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -25,13 +32,22 @@ const App = () => {
     setNewName(event.target.value)
   }
 
-  const displayName = persons.map((person, index) => <li key={index}>{person.name}</li>)
+  const handleNumberChange = (event) =>{
+    setNewNumber(event.target.value)
+  }
+
+  const displayName = persons.map((person, index) => <li key={index}>{person.name} {person.number}</li> )
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addName}>
         <div>
-          <input value={newName} onChange={handleNameChange}/>
+          <label htmlFor="name">Name: </label>
+          <input value={newName} onChange={handleNameChange}></input>
+        </div>
+        <div>
+          <label htmlFor="name">Number: </label>
+          <input value={newNumber} onChange={handleNumberChange}></input>
         </div>
         <div>
           <button type="submit">add</button>
