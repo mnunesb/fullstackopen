0ea8f7fecb509dd33fcf2057463ded99a9610812
Marcello Bernardes <mnunesbernardes@gmail.com>
2024-07-23 17:18:02 +0100

    added search functionality

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {name: 'Arto Hellas', number: 2498771125}
+    {name: 'Arto Hellas', number: 2498771125},
+    {name: 'Barto Hellas', number: 12498771125},
+    {name: 'Carto Hellas', number: 2498771125},
+    {name: 'Marcello Nunes', number: 2498776125},
+    {name: 'Darcy Ribeiro', number: 998771125},
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
 
   const addName = (event) =>{
     event.preventDefault();
@@ -13,10 +18,10 @@ const App = () => {
       alert("name is empty or is already included")
       setNewName('')
       return(0);}
-        if(find(newNumber) === true || (newNumber) === ''){
-          alert("Number is empty or is already included")
-          setNewNumber('')
-          return(0);
+    if(find(newNumber) === true || (newNumber) === ''){
+      alert("Number is empty or is already included")
+      setNewNumber('')
+      return(0);
       } else{
       const personObject = {
         name: newName,
@@ -35,13 +40,20 @@ const App = () => {
   const handleNumberChange = (event) =>{
     setNewNumber(event.target.value)
   }
+  
+  const handleSearch = (event) =>{
+    setNewSearch(event.target.value)
+    console.log({newSearch});
+  }
 
-  const displayName = persons.map((person, index) => <li key={index}>{person.name} {person.number}</li> )
+  const displayName = persons.filter(person => person.name.toLowerCase().includes(newSearch.toLowerCase())).map((person, index) => <li key={index}>{person.name} {person.number}</li>);
+
   
   return (
     <div>
       <h1>Phonebook</h1>
-
+      <label htmlFor="search">Search: </label>
+      <input value={newSearch} onChange={handleSearch}></input>
       <form onSubmit={addName}>
         <h2>Add new contact</h2>
         <div>
@@ -57,9 +69,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {displayName}
-      </ul>
+        <ul> {displayName} </ul>
+
     </div>
   )
 }
