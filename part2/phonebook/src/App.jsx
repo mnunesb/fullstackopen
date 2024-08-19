93746f51bf3e70479da21002c12b5f3e286022c6
Marcello Bernardes <mnunesbernardes@gmail.com>
2024-08-19 17:01:49 +0100

    exercise 2.11

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,20 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Search from './components/Search';
 import Form from './components/Form'
 import DisplayInfo from './components/DisplayInfo'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: 2498771125 },
-    { name: 'Barto Hellas', number: 12498771125 },
-    { name: 'Marcello Nunes', number: 2498776125 },
-    { name: 'Darcy Ribeiro', number: 998771125 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [newSearch, setNewSearch] = useState('');
 
- 
+useEffect(()=>{
+  axios
+    .get('http://localhost:3001/persons')
+    .then(response=>{
+      setPersons(response.data)
+    })
+},[]);
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
